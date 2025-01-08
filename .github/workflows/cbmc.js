0d5d6461e0801f302bd77d779a00705cb583b214
zeroninefourfour <zwec181818@proton.me>
2025-01-08 20:22:40 +0800

    重寫結構中
    # 正在重寫中. CBMC短時間內壞掉

--- .github/workflows/cbmc.js
+++ .github/workflows/cbmc.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const axios = require("axios")
 var HttpsProxyAgent = require('https-proxy-agent');
+const { algoliasearch } = require('algoliasearch');
 
 var posts = {}
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
@@ -15,26 +16,28 @@ const axget = async (url,config) => {
     }
  }
 }
+const api = "tw3.shdtw.cloud:20016"
+
 const fetchLatest = async () => {
     const latestData = await fs.readFileSync("./info.json")
     const oldDataFile = await  fs.readFileSync("./posts.json")
     const oldData = JSON.parse(oldDataFile)
     const latest = JSON.parse(latestData)
-    const proxy =  new HttpsProxyAgent.HttpsProxyAgent(`http://160.86.242.23:8080`)
+   //  const proxy =  new HttpsProxyAgent.HttpsProxyAgent(`http://160.86.242.23:8080`)
     await wait(1000)
     console.log("Connection Emstablished")
-    const latestPost = await axget('https://api.cbmc.club/v1/latest?limit=1',{
-        httpsAgent: proxy
+    const latestPost = await axget(`http://${api}/v1/latest?limit=1`,{
+      //  httpsAgent: proxy
     })
     console.log("Fetched Latest Post")
-    const json = latestPost.data
+     const json = latestPost.data
     const id = json.posts["1"].post.id.platform
     //
     for (let i = latest.totalPosts; i < id;i++){
         console.log("Fetching Post " + i)
         try {
-            const post = await axget(`https://api.cbmc.club/v1/post/${i}`,{
-                httpsAgent:  proxy
+            const post = await axget(`http://${api}/v1/post/${i}`,{
+                //httpsAgent:  proxy
         
                  
             }) 
@@ -45,12 +48,52 @@ const fetchLatest = async () => {
             posts[i] = error.data
         }
     }
-    const LatestPost = await axget("https://api.cbmc.club/v1/latest?limit=300",{
-                httpsAgent:  proxy
+    const LatestPost = await axget(`http://${api}/v1/latest?limit=300`,{
+              //  httpsAgent:  proxy
            
             })            
     fs.writeFileSync('latestPost.json',JSON.stringify(LatestPost.data))
     fs.writeFileSync('posts.json', JSON.stringify(Object.assign(oldData,posts)))
     fs.writeFileSync('info.json', JSON.stringify({totalPosts: id}))
+    return Object.assign(oldData,posts)
 }
-fetchLatest()
+const updateAlgolia = (data) =>{
+    const client = algoliasearch('76O5YAWFNC', 'ec74ea084859bbb8c72367e4d9a67464');
+    const formatted = []
+    console.log(data)
+    for (const [key, value] of Object.entries(data)) {
+         console.log(value)
+        if(value.status == "success") {
+            const post = value.posts["1"].post
+            formatted.push({
+                content: post.content,
+                type: post.type,
+                id: post.id.platform
+            })
+        };
+         
+    }
+     const chunked = []
+    // split every 10kb
+    while (formatted.length > 0) {
+        chunked.push(formatted.splice(0, 25))
+    }
+ 
+    // upload to algolia
+     chunked.forEach(async (chunk) => {
+        await client.saveObjects({
+            indexName: 'Posts',
+            objects: chunk
+        })
+    })
+    console.log(`
+        Result: ${chunked.length * 35} posts uploaded to Algolia
+        Uploaded at: ${new Date().toISOString()}
+        Saved ${chunked.length} times
+     `)
+
+ }
+(async () => {
+    const data = await fetchLatest()
+    updateAlgolia(data)
+})()
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # 靠杯資料庫
 用來做搜尋功能或許很有用.
 使用GH Actions更新資料
+
+## 功能
+- 自動保存最新資訊
+- 自動更新Algolia
+
+## `autofix.json`
+禁詞列表,如果有建議可透過GitHub PR或是透過`靠北+`內建禁詞建議功能
+## `info.json`
+處存自動獲取的統計資料
+## `latestPost.json`
+最新文章
+## `posts.json`
+全部文章
